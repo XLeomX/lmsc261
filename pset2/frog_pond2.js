7f942a3aa7e9674932c0b7655318d80a6b07535f
XLeomX <xleom6@gmail.com>
2026-09-18 12:19:32 -0400

    update

--- pset2/frog_pond2.js
+++ pset2/frog_pond2.js
@@ -1,5 +1,5 @@
 const activities = ["babysit tadpoles", "flies for lunch", "tongue stretch", "swimming lesson"];
-let activity1 = prompt("choose an activity for the frogs to do today");
-let Index = activity1 % activities.length;
+let Index = prompt("choose an activity for the frogs to do today");
+Index = Index % 4;
 print(activities[Index]);
 
